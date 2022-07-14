12776b77af11b871b415c78a8026f9d2e1d52887
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2022-07-13 23:14:06 -0400

    Update invoke.sh

--- invoke.sh
+++ invoke.sh
@@ -7,7 +7,7 @@ curl  --request POST \
 ## If you want to orchestrate this all via the AWS CLI here is how you can create a Lambda function and function URL via shell
 
 #Creating a Lambda function with zip-file
-aws lambda create-function --function-name <<lambda-function name>> --role <<Insert Lambda Role ARN>> 
+aws lambda create-function --function-name "lambda-function name" --role <<Insert Lambda Role ARN>> 
 --runtime python3.9 --zip-file <<Zip file with code>> --handler <<File with handler func>>
 
 
